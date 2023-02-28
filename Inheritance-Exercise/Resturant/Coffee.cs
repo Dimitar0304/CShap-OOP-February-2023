43f2e94b46afb3928bf6001be205e4aa64c74ed5
Димитър Банчев <114139864+Dimitar0304@users.noreply.github.com>
2023-02-28 19:25:55 +0200

    Add files via upload

---
+++ Inheritance-Exercise/Resturant/Coffee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Coffee:HotBeverage
+    {
+        public Coffee(string name,decimal price,double mililitres):base(name,price,mililitres)
+        {
+            
+        }
+        public double Coffeine { get; set; }
+        private const decimal CoffeePrice = 3.5m;
+        private const double CoffeeMilliliters = 50;
+    }
+}
